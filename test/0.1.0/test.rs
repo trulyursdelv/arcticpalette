dce3b0bfe0a347deafea96f1f3ff1173f9f2b152
trulyursdelv <151720755+trulyursdelv@users.noreply.github.com>
2025-04-26 15:35:11 +0800

    Create test.rs

---
+++ test/0.1.0/test.rs
@@ -0,0 +1,14 @@
+// WARNING:
+// This file is not implemented yet.
+
+use arcticpalette::{ArcticPalette, Command}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  pub fn init_test() {
+    let _arctic = ArcticPalette::new();
+    // TODO: ArcticPalette needs to be test-compatible.
+  }
+}
